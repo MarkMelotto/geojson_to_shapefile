92694f242c49410496ca90c65c823e9312a7952d
Mark Melotto <markmelotto@tudelft.nl>
2025-03-25 10:11:06 +0100

    updated the code to save to basin_id as a folder

--- geojson_to_shapefile.py
+++ geojson_to_shapefile.py
@@ -1,11 +1,13 @@
 import geopandas as gpd
 
 
-def convert_geojson_to_shapefile(input_geojson, output_shapefile):
+def convert_geojson_to_shapefile(input_geojson, output_folder):
     # Read the GeoJSON file
     gdf = gpd.read_file(input_geojson)
+    grdc_station_number = int(gdf["grdc_no"])
 
-    # Save it as a Shapefile
+    # Save it as a Shapefile in the folder of the basin_id
+    output_shapefile = output_folder + "/" + str(grdc_station_number)
     gdf.to_file(output_shapefile, driver='ESRI Shapefile')
 
     print(f"Shapefile saved to {output_shapefile}")
@@ -13,5 +15,5 @@ def convert_geojson_to_shapefile(input_geojson, output_shapefile):
 
 # Example usage
 input_geojson = "geojsons/stationbasins.geojson"  # Change to your input file path
-output_shapefile = "shapefiles/output_shapefile"  # Change to your desired output directory (without extension)
-convert_geojson_to_shapefile(input_geojson, output_shapefile)
+output_folder = "shapefiles"  # Change to your desired output directory (without extension)
+convert_geojson_to_shapefile(input_geojson, output_folder)
