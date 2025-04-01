49028950e98163877143760d7965bc91d9f57375
Mark Melotto <markmelotto@tudelft.nl>
2025-04-01 10:14:09 +0200

    added data for Ghana Kenya and Zimbabwe

--- geojson_to_shapefile.py
+++ geojson_to_shapefile.py
@@ -1,19 +1,20 @@
 import geopandas as gpd
 
 
-def convert_geojson_to_shapefile(input_geojson, output_folder):
+def convert_station_basin_geojson_to_shapefile(input_geojson, output_folder, country):
     # Read the GeoJSON file
     gdf = gpd.read_file(input_geojson)
-    grdc_station_number = int(gdf["grdc_no"])
+    # grdc_station_number = int(gdf["grdc_no"])
 
     # Save it as a Shapefile in the folder of the basin_id
-    output_shapefile = output_folder + "/" + str(grdc_station_number)
+    output_shapefile = output_folder + "/" + str(country)
     gdf.to_file(output_shapefile, driver='ESRI Shapefile')
 
     print(f"Shapefile saved to {output_shapefile}")
 
 
 # Example usage
-input_geojson = "geojsons/stationbasins.geojson"  # Change to your input file path
-output_folder = "shapefiles"  # Change to your desired output directory (without extension)
-convert_geojson_to_shapefile(input_geojson, output_folder)
+country = "Zimbabwe"
+input_geojson = "geojsons/" + country + "/stationbasins.geojson"  # Change to your input file path
+output_folder = "shapefiles/" + country  # Change to your desired output directory (without extension)
+convert_station_basin_geojson_to_shapefile(input_geojson, output_folder, country)
